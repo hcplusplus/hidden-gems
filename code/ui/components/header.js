74fdf81ba1b94a26ca8dfc0253e88a1e5ac0f655
hannah <hmoore@berkeley.edu>
2025-03-31 20:35:54 -0700

    next step: rework to simple react framework

---
+++ code/ui/components/header.js
@@ -0,0 +1,25 @@
+// Header component that can be included in all pages
+document.write(`
+    <div class="title-bar">
+      <div class="title-bar-text">NorCal Hidden Gems</div>
+      <div class="title-bar-controls">
+        <button aria-label="Minimize"></button>
+        <button aria-label="Maximize"></button>
+        <button aria-label="Close"></button>
+      </div>
+    </div>
+    <div class="header-content">
+      <marquee behavior="scroll" direction="left">***DISCOVER PLACES THAT TOURISTS DON'T KNOW ABOUT***</marquee>
+      
+      <div class="menu">
+        <img src="construction.gif" height="30" alt="Under Construction">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="map.html">Vintage Map</a></li>
+          <li><a href="places.html">Special Places</a></li>
+          <li><a href="about.html">About</a></li>
+        </ul>
+        <img src="construction.gif" height="30" alt="Under Construction">
+      </div>
+    </div>
+  `);
